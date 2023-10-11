b9cd6da433e29060f9c625ddb0c960e5d2bb0a28
DERA10 <145374634+DERA10@users.noreply.github.com>
2023-10-11 18:58:20 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,10 +21,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['916909137213']
-global.premium = ['916909137213']
-global.ownernomer = '916909137213'
-global.ownername = '🦄Dream Guy Xeon'
+global.owner = ['2349067102251']
+global.premium = ['2349067102251']
+global.ownernomer = '2349067102251'
+global.ownername = '🦄Dream Guy Dux'
 global.botname = 'Cheems Bot MD V2'
 global.footer = 'Xeon Bot Inc.'
 global.ig = 'https://github.com/DGXeon'
